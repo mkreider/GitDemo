8c5a1c720789b4663958a4f3ad14e080db3d4df5
Mathias Kreider <m.kreider@gsi.de>
2012-11-06 11:27:54 +0100

    - added heartbeat LED on Pin C6 - added comments

--- myLittleProject/myLittleProject/myLittleProject.c
+++ myLittleProject/myLittleProject/myLittleProject.c
@@ -12,30 +12,34 @@
 
 void init(void)
 {
+	//use ports B and D to show what the ADC read
 	DDRD	= 0xff;
 	DDRB	= 0xff;
-	DDRC	= 0x00;
+	DDRC	= (1<<PC6); // only C6 is output
 	
 	PORTB	= 0x00;
-	PORTC	= 0x00;
+	PORTC	= 0x00;	//no pullup for PORTC
 	PORTD	= 0x00;
+	
 	adc_init();
 	
 }
 
 int main(void)
 {
-    
-	init();
+    uint16_t value;
 	
-	uint16_t value;
+	init();
 	
 	while(1)
     {
-        value = adc_read(0);
+        //read ADC every half second
+		
+		value = adc_read(0);
 		PORTB = (uint8_t)(value & 0x00ff);
 		PORTD = (uint8_t)(value>>8);
-		_delay_ms(300);
+		_delay_ms(500);
+		PORTC ^= (1<<PC6);  // toggle PC6
     }
 }
 
