db50534049b52e9b3d516ba86d0d3e5651f6936a
Mathias Kreider <m.kreider@gsi.de>
2012-11-06 11:49:26 +0100

    started implementation of continous adc read functions

--- myLittleProject/myLittleProject/adc.c
+++ myLittleProject/myLittleProject/adc.c
@@ -41,3 +41,10 @@ uint16_t adc_read( uint8_t channel )
 // Value: ADCW = Vin * 1024 / Vref  
 	return ADCW;                    // ADC auslesen und zurückgeben
 }
+
+/* kontinuierliche ADC Messung starten */
+void adc_start ( uint8_t channel )
+{
+
+}
+
